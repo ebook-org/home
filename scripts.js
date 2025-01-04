d685a24800909ba3d4542b468fc4fe21b26e5f3b
EBOOK.ORG <ebook.org.oficial@gmail.com>
2025-01-04 00:14:29 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -68,3 +68,72 @@ searchInput.addEventListener('input', debounceInputHandler(function (event) {
 
     noResultsMessage.style.display = hasResults ? 'none' : 'block';
 }, 300));
+
+// Modal Termos de Uso
+const modal = document.getElementById('modal-termos');
+const openModalButton = document.getElementById('open-modal');
+const closeModalButton = document.getElementById('close-modal');
+
+// Abrir modal
+openModalButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.style.display = 'flex';
+});
+
+// Fechar modal
+closeModalButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Fechar modal clicando fora dele
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+// Modal Política de Privacidade
+const privacyModal = document.getElementById('modal-privacidade');
+const openPrivacyModalButton = document.getElementById('open-privacy-modal');
+const closePrivacyModalButton = document.getElementById('close-privacy-modal');
+
+// Abrir modal de Política de Privacidade
+openPrivacyModalButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    privacyModal.style.display = 'flex';
+});
+
+// Fechar modal de Política de Privacidade
+closePrivacyModalButton.addEventListener('click', () => {
+    privacyModal.style.display = 'none';
+});
+
+// Fechar modal clicando fora
+window.addEventListener('click', (event) => {
+    if (event.target === privacyModal) {
+        privacyModal.style.display = 'none';
+    }
+});
+
+// Modal Contato
+const contactModal = document.getElementById('modal-contato');
+const openContactModalButton = document.getElementById('open-contact-modal');
+const closeContactModalButton = document.getElementById('close-contact-modal');
+
+// Abrir modal de Contato
+openContactModalButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    contactModal.style.display = 'flex';
+});
+
+// Fechar modal de Contato
+closeContactModalButton.addEventListener('click', () => {
+    contactModal.style.display = 'none';
+});
+
+// Fechar modal clicando fora
+window.addEventListener('click', (event) => {
+    if (event.target === contactModal) {
+        contactModal.style.display = 'none';
+    }
+});
